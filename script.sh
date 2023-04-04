29eb37183504924b2cc8519cc190481f8b138284
이창혁 <ch22@twinny.co.kr>
2023-04-04 15:01:21 +0900

    yarn path

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
-YARN_PATH=$(yarn global bin)
-
-run_and_log() {
+run_and_log {
   local command=$1
   local log_file=$2
+  local yarn_path="$HOME/.yarn/bin/yarn"
 
-  $YARN_PATH/run $command 2>&1 | tee $log_file
+  $yarn_path run $command 2>&1 | tee "$log_file"
+
   if [ "${PIPESTATUS[0]}" -ne 0 ]; then
     echo "$command failed. Check $log_file for more details."
     exit 1
