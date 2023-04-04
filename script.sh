f0c73eb8bb5e52281c367800c3d9003bca3a941b
이창혁 <ch22@twinny.co.kr>
2023-04-04 14:53:04 +0900

    try

--- script.sh
+++ script.sh
@@ -1,10 +1,12 @@
-function run_and_log {
-  ls
-  CMD="$1"
-  LOGFILE="$2"
-  $CMD 2>&1 | tee $LOGFILE
+YARN_PATH=$(yarn global bin)
+
+run_and_log() {
+  local command=$1
+  local log_file=$2
+
+  $YARN_PATH/run $command 2>&1 | tee $log_file
   if [ "${PIPESTATUS[0]}" -ne 0 ]; then
-    echo "$CMD failed."
+    echo "$command failed. Check $log_file for more details."
     exit 1
   fi
 }
